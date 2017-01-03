c6b8928b7629bc63ae3bda9bce0d32996885026d
codebycody <cody@archonweb.com>
2017-01-03 16:44:20 -0500

    find company by mac address

---
+++ analysis/mac_id/macId.py
@@ -0,0 +1,12 @@
+import sys
+
+print(sys.argv[1])
+
+#open file
+with open('oui.txt') as f:
+	lines = f.readlines()
+
+for line in lines:
+	if line.split(' ')[0] == sys.argv[1]:
+		print(line.rstrip('\n'))
+		exit()
