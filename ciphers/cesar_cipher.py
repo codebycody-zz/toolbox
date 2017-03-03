62cfe652130db9dedc484006444301176461d61b
Cody Greene <cody@archonweb.com>
2017-03-02 23:54:21 -0500

    update tab to 4 spaces
    in accordance to pep 8

--- ciphers/cesar_cipher.py
+++ ciphers/cesar_cipher.py
@@ -8,22 +8,22 @@ args = parser.parse_args()
 strs = 'abcdefghijklmnopqrstuvwxyz'
 
 def shifttext(shift):
-	inp  = args.s
-	data = []
-	#iterate over the text not some list
-	for i in inp:
-		# if the char is not a space ""
-		if i.strip() and i in strs:
-			data.append(strs[(strs.index(i) + shift) % 26])
-		else:
-			#if space the simply append it to data
-			data.append(i)
-	output = ''.join(data)
-	return output
+    inp  = args.s
+    data = []
+    #iterate over the text not some list
+    for i in inp:
+        # if the char is not a space ""
+        if i.strip() and i in strs:
+            data.append(strs[(strs.index(i) + shift) % 26])
+        else:
+            #if space the simply append it to data
+            data.append(i)
+    output = ''.join(data)
+    return output
 
 def main():
-	print(shifttext(args.n))
+    print(shifttext(args.n))
 
 if __name__ == '__main__':
-	main()
+    main()
 
