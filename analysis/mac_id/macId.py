69c324c2142a2ccbd73eb489a23f8d3f9810fc69
Cody Greene <cody@archonweb.com>
2017-03-03 22:54:28 -0500

    update to match PEP 8 standard

--- analysis/mac_id/macId.py
+++ analysis/mac_id/macId.py
@@ -4,9 +4,9 @@ print(sys.argv[1])
 
 #open file
 with open('oui.txt') as f:
-	lines = f.readlines()
+    lines = f.readlines()
 
 for line in lines:
-	if line.split(' ')[0] == sys.argv[1]:
-		print(line.rstrip('\n'))
-		exit()
+    if line.split(' ')[0] == sys.argv[1]:
+        print(line.rstrip('\n'))
+        exit()
