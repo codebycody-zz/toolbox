a44a4762667dc563812e1b3511670d2e9a253ce9
Cody Greene <cody@archonweb.com>
2017-03-04 23:12:41 -0500

    updates to PEP 8 standards

--- analysis/traffic_analysis/ftpSniffer.py
+++ analysis/traffic_analysis/ftpSniffer.py
@@ -5,31 +5,31 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
 def ftpSniff(pkt):
-	dest = pkt.getlayer(IP).dst
-	raw = pkt.sprintf('%Raw.load%')
-	user = re.findall('(?i)USER (.*)', raw)
-	pswd = re.findall('(?i)PASS (.*)', raw)
+    dest = pkt.getlayer(IP).dst
+    raw = pkt.sprintf('%Raw.load%')
+    user = re.findall('(?i)USER (.*)', raw)
+    pswd = re.findall('(?i)PASS (.*)', raw)
 
-	if user:
-		print('[*] Detected FTP Login to ' + str(dest))
-		print('[+] User Account: ' + str(user[0]))
-	elif pswd:
-		print('[+] Password: ' + str(pswd[0]))
+    if user:
+        print('[*] Detected FTP Login to ' + str(dest))
+        print('[+] User Account: ' + str(user[0]))
+    elif pswd:
+        print('[+] Password: ' + str(pswd[0]))
 
 def main():
-	parser = optparse.OptionParser('usage %prog -i <interface>')
-	parser.add_option('-i', dest='interface', type='string', help='specify interface to listen on')
-	(options, args) = parser.parse_args()
-	if options.interface == None:
-		print(parser.usage)
-		exit(0)
-	else:
-		conf.ifave = options.interface
+    parser = optparse.OptionParser('usage %prog -i <interface>')
+    parser.add_option('-i', dest='interface', type='string', help='specify interface to listen on')
+    (options, args) = parser.parse_args()
+    if options.interface == None:
+        print(parser.usage)
+        exit(0)
+    else:
+        conf.ifave = options.interface
 
-	try:
-		sniff(filter='tcp port 21', prn=ftpSniff)
-	except KeyboardInterrupt:
-		exit(0)
+    try:
+        sniff(filter='tcp port 21', prn=ftpSniff)
+    except KeyboardInterrupt:
+        exit(0)
 
 if __name__ == '__main__':
-	main()
+    main()
